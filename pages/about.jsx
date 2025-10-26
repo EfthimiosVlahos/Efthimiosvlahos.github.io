c175c39e092245640b40f64964fb71779a1964a4
EfthimiosVlahos <efthimios.vlahos@stonybrook.edu>
2025-10-26 17:17:38 -0400

    updated resume

--- pages/about.jsx
+++ pages/about.jsx
@@ -2,7 +2,8 @@ import { pdfjs } from 'react-pdf';
 import dynamic from 'next/dynamic';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
-const myResume = 'pages/Efthimios_Vlahos_Resume_Jul25.pdf';
+const myResume = 'public/Efthimios_Vlahos_Resume_Oct25.pdf';
+
 
 
 // Dynamically import the Document and Page components
@@ -12,26 +13,26 @@ const Page = dynamic(() => import('react-pdf').then((mod) => mod.Page), { ssr: f
 const AboutPage = () => {
   return (
     <>
-<h3>About Me</h3><br/>
-<ul>
-  <li>💻 Currently driving business intelligence initiatives as a Machine Learning Engineer at Con Ed, specializing in predictive model development and data pipeline architecture.</li>
-  <li>📖 Master of Science in Applied Mathematics & Statistics from SUNY Stony Brook University, with a strong focus on data science, machine learning, and business analytics.</li>
-  <li>🎓 Research Focus: Actively engaged in research on AI, MLOps, and cloud infrastructure, developing scalable solutions for real-world business problems.</li>
-  <li>💪 Fitness Devotee: Regularly hitting the gym to stay fit, valuing both physical and mental well-being: proudly have never skipped leg day 🦵.</li>
-  <li>📚 Philosophy Enthusiast: Passionate about reading and discussing philosophical ideas and literature.</li>
-</ul>
+      <h3>About Me</h3><br />
+      <ul>
+        <li>💻 Currently driving business intelligence initiatives as a Machine Learning Engineer at Con Ed, specializing in predictive model development and data pipeline architecture.</li>
+        <li>📖 Master of Science in Applied Mathematics & Statistics from SUNY Stony Brook University, with a strong focus on data science, machine learning, and business analytics.</li>
+        <li>🎓 Research Focus: Actively engaged in research on AI, MLOps, and cloud infrastructure, developing scalable solutions for real-world business problems.</li>
+        <li>💪 Fitness Devotee: Regularly hitting the gym to stay fit, valuing both physical and mental well-being: proudly have never skipped leg day 🦵.</li>
+        <li>📚 Philosophy Enthusiast: Passionate about reading and discussing philosophical ideas and literature.</li>
+      </ul>
 
 
 
       <br></br>
       <center>
-        <h3>Resume (<a href={myResume} download='pages/Efthimios_Vlahos_Resume_Jul25.pdf'>Download</a>)</h3>
+        <h3>Resume (<a href={myResume} download='public/Efthimios_Vlahos_Resume_Oct25.pdf'>Download</a>)</h3>
         <br />
-        <Document 
-          file={myResume} 
+        <Document
+          file={myResume}
           onLoadError={(error) => console.error('PDF load error:', error)}
         >
-          <Page pageIndex={0} renderMode="svg"/>
+          <Page pageIndex={0} renderMode="svg" />
           {/* Only include this if your PDF has a second page */}
           {/* <Page pageIndex={1} renderMode="svg"/> */}
         </Document>
